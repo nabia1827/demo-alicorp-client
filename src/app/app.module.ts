3d1d24c803eb81216666a994f20ee87747ca0360
nabia1827 <w1ck3d.ig@gmail.com>
2026-06-25 12:22:07 -0500

    FEAT_project_structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './modules/auth/login/login.component';
+import { ProductListComponent } from './modules/products/product-list/product-list.component';
+import { ProductFormComponent } from './modules/products/product-form/product-form.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { LoadingComponent } from './shared/components/loading/loading.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProductListComponent,
+    ProductFormComponent,
+    NavbarComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
